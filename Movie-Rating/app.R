95b360152751357a77362902e11108407fddb2b6
Mohammed Ali <mali@uplandsoftware.com>
2018-11-17 13:12:02 +0200

    adding movies table

--- Movie-Rating/app.R
+++ Movie-Rating/app.R
@@ -1,67 +1,99 @@
 library(shiny)
 library(ggplot2)
-load(url("http://s3.amazonaws.com/assets.datacamp.com/production/course_4850/datasets/movies.Rdata"))
+library(DT)
+library(dplyr)
+load(
+  url(
+    "http://s3.amazonaws.com/assets.datacamp.com/production/course_4850/datasets/movies.Rdata"
+  )
+)
+n_total <- nrow(movies)
 
 # Define UI for application that plots features of movies
-ui <- fluidPage(
-  
-  # Sidebar layout with a input and output definitions
+ui <- fluidPage(# Sidebar layout with a input and output definitions
   sidebarLayout(
-    
     # Inputs
     sidebarPanel(
-      
       # Select variable for y-axis
-      selectInput(inputId = "y", 
-                  label = "Y-axis:",
-                  choices = c("IMDB rating"          = "imdb_rating", 
-                              "IMDB number of votes" = "imdb_num_votes", 
-                              "Critics score"        = "critics_score", 
-                              "Audience score"       = "audience_score", 
-                              "Runtime"              = "runtime"), 
-                  selected = "audience_score"),
+      selectInput(
+        inputId = "y",
+        label = "Y-axis:",
+        choices = c(
+          "IMDB rating"          = "imdb_rating",
+          "IMDB number of votes" = "imdb_num_votes",
+          "Critics score"        = "critics_score",
+          "Audience score"       = "audience_score",
+          "Runtime"              = "runtime"
+        ),
+        selected = "audience_score"
+      ),
       
       # Select variable for x-axis
-      selectInput(inputId = "x", 
-                  label = "X-axis:",
-                  choices = c("IMDB rating"          = "imdb_rating", 
-                              "IMDB number of votes" = "imdb_num_votes", 
-                              "Critics score"        = "critics_score", 
-                              "Audience score"       = "audience_score", 
-                              "Runtime"              = "runtime"), 
-                  selected = "critics_score"),
+      selectInput(
+        inputId = "x",
+        label = "X-axis:",
+        choices = c(
+          "IMDB rating"          = "imdb_rating",
+          "IMDB number of votes" = "imdb_num_votes",
+          "Critics score"        = "critics_score",
+          "Audience score"       = "audience_score",
+          "Runtime"              = "runtime"
+        ),
+        selected = "critics_score"
+      ),
       
       # Select variable for color
-      selectInput(inputId = "z", 
-                  label = "Color by:",
-                  choices = c("Title type" = "title_type", 
-                              "Genre" = "genre", 
-                              "MPAA rating" = "mpaa_rating", 
-                              "Critics rating" = "critics_rating", 
-                              "Audience rating" = "audience_rating"),
-                  selected = "mpaa_rating"),
+      selectInput(
+        inputId = "z",
+        label = "Color by:",
+        choices = c(
+          "Title type" = "title_type",
+          "Genre" = "genre",
+          "MPAA rating" = "mpaa_rating",
+          "Critics rating" = "critics_rating",
+          "Audience rating" = "audience_rating"
+        ),
+        selected = "mpaa_rating"
+      ),
       # Set alpha level
-      sliderInput(inputId = "alpha", 
-                  label = "Alpha:", 
-                  min = 0, max = 1, 
-                  value = 0.5)
+      sliderInput(
+        inputId = "alpha",
+        label = "Alpha:",
+        min = 0,
+        max = 1,
+        value = 0.5
+      ),
+      # Text instructions
+      HTML(paste("Enter a value between 1 and", n_total)),
+      
+      # Numeric input for sample size
+      numericInput(
+        inputId = "n",
+        label = "Sample size:",
+        value = 30,
+        step = 1,
+        min = 1,
+        max = n_total
+      )
     ),
     
     # Outputs
     mainPanel(
       plotOutput(outputId = "scatterplot"),
-      plotOutput(outputId = "densityplot", height = 200)
+      plotOutput(outputId = "densityplot", height = 200),
+      DT::dataTableOutput(outputId = "moviestable")
     )
-  )
-)
+  ))
 
 # Define server function required to create the scatterplot
 server <- function(input, output) {
-  
   # Create the scatterplot object the plotOutput function is expecting
   output$scatterplot <- renderPlot({
-    ggplot(data = movies, aes_string(x = input$x, y = input$y,
-                                     color = input$z)) +
+    ggplot(data = movies, aes_string(
+      x = input$x,
+      y = input$y,
+      color = input$z
+    )) +
       geom_point(alpha = input$alpha)
   })
   
@@ -69,6 +101,18 @@ server <- function(input, output) {
   output$densityplot <- renderPlot({
     ggplot(data = movies, aes_string(x = input$x)) +
       geom_density()
+  })
+  
+  # Create data table
+  output$moviestable <- DT::renderDataTable({
+    movies_sample <- movies %>%
+      sample_n(input$n) %>%
+      select(title:studio)
+    DT::datatable(
+      data = movies_sample,
+      options = list(pageLength = 10),
+      rownames = FALSE
+    )
   })
 }
 
